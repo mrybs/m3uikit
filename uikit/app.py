24910323def99a4a2e621c73c607dd68416832e3
mrybs <mrybs@github.com>
2024-04-02 00:05:07 +0300

    020424A

--- uikit/app.py
+++ uikit/app.py
@@ -2,77 +2,26 @@ from slinn import Dispatcher, LinkFilter, HttpResponse
 from ntml.ntmlc import ntml_compile
 
 meals = {
-	'br': {'tag': 'br'},
-	'Button': {'tag': 'button', 'props':({'style': """
-		background: #C2E7FF;
-		color: #000000;
-		padding: 20px;
-		border-radius: 20px;
-		border: 0;
-		font-size: 16px;
-		text-align: center;
-		"""}, [])},
-	'CButton': {'tag': 'button', 'props':({'style': """
-		background: #C2E7FF;
-		color: #000000;
-		padding: auto;
-		border-radius: 40px;
-		border: 0;
-		font-size: 24px;
-		font-weight: bold;
-		width: 64px;
-		height: 64px;
-		margin: 3px;
-		text-align: center;
-		"""}, [])},
-	'MButton': {'tag': 'button', 'props':({'style': """
-		background: #00000000;
-		color: #000000;
-		padding: auto;
-		border-radius: 40px;
-		border: 0;
-		font-size: 20px;
-		font-weight: bold;
-		width: 55px;
-		height: 35px;
-		margin: 3px;
-		text-align: center;
-		"""}, [])},
-	'SButton': {'tag': 'button', 'props':({'style': """
-		background: #C2E7FF;
-		color: #000000;
-		padding: auto;
-		border-radius: 40px;
-		border: 0;
-		font-size: 14px;
-		font-weight: bold;
-		width: 26px;
-		height: 26px;
-		margin: 3px;
-		text-align: center;
-		"""}, [])},
-	'LInput': {'tag': 'input', 'props': ({'style': """
-		background: #C2E7FF;
-		color: #000000;
-		width: calc(100% - 1px);
-		height: 125px;
-		border: 0;
-		margin: 0;
-		padding: 0;
-		outline: 0;
-		text-align: right;
-		font-size: 48px;
-		border-bottom-left-radius: 20px;
-		border-bottom-right-radius: 20px;
-		"""}, [])},
-	'ThreeDots': {'tag': 'button', 'props': ({'style':"""
-		content: '︙';
-		"""}, [])}
+	'Button': {'tag': 'button', 'props':({'class': 'Button Button-animation'}, [])},
+	'CButton': {'tag': 'button', 'props':({'class': 'CButton Button-animation'}, [])},
+	'MButton': {'tag': 'button', 'props':({'class': 'MButton Button-animation'}, [])},
+	'SButton': {'tag': 'button', 'props':({'class': 'SButton Button-animation'}, [])},
+	'CalculatorInput': {'tag': 'input', 'props': ({'class': 'CalculatorInput'}, [])},
+	'ThreeDots': {'tag': 'button', 'props': ({'class': 'ThreeDots Button-animation'}, [])},
+	'Container': {'tag': 'div', 'props': ({'class': 'Container'}, [])}
 }
  
 dp = Dispatcher()
 
 # Write your code down here   
+@dp(LinkFilter('m3uikit.css'))
+def styles(request):
+	return render('m3uikit.css')
+
+@dp(LinkFilter('m3uikit_color.css'))
+def styles(request):
+	return render('m3uikit_color.css')
+
 @dp(LinkFilter('styles.css'))
 def styles(request):
 	return render('styles.css')
